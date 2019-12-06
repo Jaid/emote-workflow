f51357ff80d33819bbbae95021d00177e5c851ea
Jaid <jaid.jsx@gmail.com>
2019-12-06 21:26:51 +0100

    Removed some unneeded code
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-import core from "./core"
-
-const job = async () => {
-  const plugins = {}
-  const pluginsRequire = require.context("./plugins/", true, /index.js$/)
-  for (const value of pluginsRequire.keys()) {
-    const {pluginName} = value.match(/[/\\](?<pluginName>.+?)[/\\]index\.js$/).groups
-    plugins[pluginName] = pluginsRequire(value).default
-  }
-  await core.init(plugins)
-}
-
-job().catch(error => {
-  console.error(error)
-  process.exit(1)
-})
+console.log("TEST")
